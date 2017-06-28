704a718d1078f545026294f4f29613ceea0413c8
Rafael <aciolyr@gmail.com>
2017-06-28 17:47:20 -0300

    arquivos header e footter movidos para root e css bulma e normaliza adicionados

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_header(); ?>
 
-<div class="hero is-fullheight">
+<div class="hero is-fullheight is-primary">
   <div class="hero-body">
     <div class="container">
       <!-- Adicionar as imagens e textos do primeiro bloco -->
@@ -8,4 +8,28 @@
   </div>
 </div>
 
-<?php get_template_part('templates/page', 'footer') ?>
+<nav class="nav">
+  <div class="container">
+    <div class="nav-left">
+      <div class="nav-item">
+        <a href="#">
+          <img src="http://placehold.it/150x150" alt="">
+        </a>
+      </div>
+    </div>
+
+    <div class="nav-right">
+      <div class="nav-item">
+        <a href="#">Item</a>
+      </div>
+      <div class="nav-item">
+        <a href="#">Item</a>
+      </div>
+      <div class="nav-item">
+        <a href="#">Item</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<?php get_footer(); ?>
