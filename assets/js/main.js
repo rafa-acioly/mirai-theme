e1e43a9204795771f3845df12f09ab064d8663ae
Rafael <aciolyr@gmail.com>
2017-06-28 17:22:23 -0300

    principais funções de scroll adicionadas ao arquivo

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('[data-scroll]').on('click', function () {
+
+    // TODO: realizar o scroll para a seção referente ao link clicado
+
+  });
+
+  $('.main-hero .scroll-down').on('click', function () {
+
+    // TODO: Realizar o scroll da pagina ate o navbar
+
+  });
+});
