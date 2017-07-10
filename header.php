b8d8351d41bb77124292ec8cd1ea873c75f5fd1d
Rafael <aciolyr@gmail.com>
2017-07-10 11:03:24 -0300

    titulo e titulo de tab ajustados

--- header.php
+++ header.php
@@ -3,7 +3,10 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= bloginfo('name'); ?></title>
+    <title>
+      <?= bloginfo('name'); ?> - 
+      <?= is_category() ? get_queried_object()->name : ''; ?>
+    </title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/normalize.css">
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/animate.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
