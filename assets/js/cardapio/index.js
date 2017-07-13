016ef5788f525ccd0bedd03410a0994ec9c633d9
Rafael <aciolyr@gmail.com>
2017-07-13 15:02:03 -0300

    busca de posts por categoria implementado

--- assets/js/cardapio/index.js
+++ assets/js/cardapio/index.js
@@ -1,13 +1,36 @@
 import vue from 'vue/dist/vue';
+import wpapi from 'wpapi';
 
 new vue({
   el: '#cardapio-app',
   data: {
     cardapioTitle: 'Cardápio',
+    isSearching: false,
+    products: [],
+    wp: ''
   },
+
+  created: function () {
+    console.log('created');
+    this.wp = new wpapi({endpoint: 'http://mirai.dev/index.php/wp-json'});
+  },
+
   methods: {
     findProducts (name) {
       this.cardapioTitle = name;
+      this.isSearching = true;
+      this.wp.categories().slug(name)
+      .then(categorie => {
+        var current = categorie[0];
+        return this.wp.posts().categories(current.id);
+      })
+      .then(productsList => {
+        productsList.forEach(product => {
+          this.products.push(product);
+        });
+        this.isSearching = false;
+        console.log(this.products);
+      });
     }
   }
 })
