c20a8d07ce2b16aab9d54c1ad015c0cca2b04fd8
Rafael <aciolyr@gmail.com>
2017-07-13 18:11:21 -0300

    pagina de plugin habilitada

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ function clean_menu() {
   remove_menu_page ('tools.php');
   // remove_menu_page ('edit.php');
   remove_menu_page ('edit-comments.php');
-  remove_menu_page ('plugins.php');
+  // remove_menu_page ('plugins.php');
   remove_menu_page ('themes.php');
   // remove_menu_page ('users.php');
 }
