16ca2be6a2d0a6d8e94d13aca24a481e530a700f
Rafael <aciolyr@gmail.com>
2017-07-31 13:14:54 -0300

    opcoes devolvidas ao tema

--- functions.php
+++ functions.php
@@ -46,8 +46,8 @@ function clean_menu() {
   remove_menu_page ('upload.php');
   remove_menu_page ('link-manager.php');
   remove_menu_page ('edit.php?post_type=page');
-  remove_menu_page ('options-general.php');
-  remove_menu_page ('tools.php');
+  // remove_menu_page ('options-general.php');
+  // remove_menu_page ('tools.php');
   // remove_menu_page ('edit.php');
   remove_menu_page ('edit-comments.php');
   // remove_menu_page ('plugins.php');
